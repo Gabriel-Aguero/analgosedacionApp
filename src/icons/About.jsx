e835d3d5f83c5ad1a876e3de47d321a8b8d65bc3
Hector Gabriel Aguero <gabrielhaguero@gmail.com>
2025-11-07 15:01:29 -0300

    update de repo

--- src/icons/About.jsx
+++ src/icons/About.jsx
@@ -1,4 +1,4 @@
-export const IconHome = () => (
+export const About = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -11,8 +11,9 @@ export const IconHome = () => (
     stroke-linejoin="round"
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-    <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-    <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+    <path d="M6 21v-2a4 4 0 0 1 4 -4h3.5" />
+    <path d="M20 21l2 -2l-2 -2" />
+    <path d="M17 17l-2 2l2 2" />
   </svg>
 );
