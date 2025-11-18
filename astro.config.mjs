c85855a69ade6444a6cff8dca5d0e8aa0041e4e4
Hector Gabriel Aguero <gabrielhaguero@gmail.com>
2025-11-18 14:14:54 -0300

    agrego manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,56 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import VitePWA from "@vite-pwa/astro";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [preact()]
+  integrations: [
+    preact(),
+    VitePWA({
+      registerType: "autoUpdate", // Actualiza el SW automáticamente
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico,webp,json}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      manifest: {
+        name: "Analgosedación",
+        short_name: "Analgosedación",
+        description: "Escalas y herramientas para analgosedación en pediatría.",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#0f172a",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/apple-180.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
