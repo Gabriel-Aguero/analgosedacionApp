a32df93698983a3df79bb43937227a31cfc295f2
Hector Gabriel Aguero <gabrielhaguero@gmail.com>
2025-11-18 14:30:03 -0300

    agrego iconos

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,12 +39,17 @@ export default defineConfig({
             type: "image/png",
           },
           {
+            src: "/icons/apple-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
             src: "/icons/icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/icons/apple-180.png",
+            src: "/icons/apple-icon-180.png",
             sizes: "180x180",
             type: "image/png",
             purpose: "any maskable",
